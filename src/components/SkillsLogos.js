32ad67f8a028f0f4d19c0765d702e2d318d7f6c4
Will-Copeland <will@localhost.localdomain>
2019-02-06 23:42:01 -0800

    started logo cluster

---
+++ src/components/SkillsLogos.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from 'react';
+import { ParallaxLayer } from 'react-spring/addons';
+
+
+
+class SkillsLogos extends PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+
+      <ParallaxLayer>
+
+      </ParallaxLayer>
+
+      <ParallaxLayer>
+
+      </ParallaxLayer>
+      </React.Fragment>
+
+    )
+  }
+}
+
+
+export default SkillsLogos;
