591a6f4ac242d3cc60eb9d86d10eca832fa79e3f
Will-Copeland <will@localhost.localdomain>
2019-04-18 20:58:50 -0700

    deleted unused code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
 import { withStyles, MuiThemeProvider } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Helmet } from 'react-helmet';
 import Theme from '../utils/Theme';
 
 const styles = {
-
 };
 
 class Layout extends React.PureComponent {
@@ -20,23 +18,13 @@ class Layout extends React.PureComponent {
 
       <CssBaseline>
         <MuiThemeProvider theme={Theme}>
-          <div className={classes.root}>
-            <div
-              style={{
-                margin: '0 auto',
-                width: '100%',
-                paddingTop: 0,
-                overflowY: 'hidden',
-              }}
-            >
-              <Helmet>
-                <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
-              </Helmet>
-              <main>{children}</main>
-              <footer />
-            </div>
-          </div>
+          <React.Fragment>
+            <Helmet>
+              <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet" />
+              <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
+            </Helmet>
+            <main>{children}</main>
+          </React.Fragment>
         </MuiThemeProvider>
       </CssBaseline>
     );
