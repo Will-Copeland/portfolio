3e8a958900b6ce279712292a51b3a020595e72f6
Will-Copeland <will@localhost.localdomain>
2019-05-01 18:52:09 -0700

    create Work

---
+++ src/components/Work.js
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react'
+import { withStyles } from '@material-ui/core';
+
+const styles = {
+
+};
+
+class Work extends PureComponent {
+  render() {
+      const { classes } = this.props
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(Work)
